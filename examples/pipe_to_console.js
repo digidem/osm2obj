9c34d265ee5c6320a98302c44b6113fe9cb4dd5b
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-08-20 19:06:25 -0700

    Ensure example works on windows

--- examples/pipe_to_console.js
+++ examples/pipe_to_console.js
@@ -1,7 +1,8 @@
 var fs = require('fs')
+var path = require('path')
 var Osm2Json = require('../lib/osm2json')
 var concat = require('concat-stream')
 
-var rs = fs.createReadStream(__dirname + '/test.osm')
+var rs = fs.createReadStream(path.join(__dirname, '/test.osm'))
 
 rs.pipe(new Osm2Json()).pipe(concat(console.log))
