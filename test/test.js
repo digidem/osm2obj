b48f264331e619a4d258bce508e518da1508655b
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-08-22 09:32:07 -0700

    New tests

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ var test = require('tape')
 var fs = require('fs')
 var path = require('path')
 var concat = require('concat-stream')
+var fromString = require('from2-string')
 
 var Osm2Json = require('../lib/osm2json')
 
@@ -12,6 +13,15 @@ test('expected output defaults', function (t) {
     t.deepEqual(data, expected)
     t.end()
   }))
+})
+
+test('non-streaming', function (t) {
+  var expected = require('./output_defaults.json')
+  var input = fs.readFileSync(path.join(__dirname, 'test.osm'), 'utf8')
+  var parser = new Osm2Json()
+  t.deepEqual(parser.parse(input), expected)
+  t.deepEqual(parser.parse(input), expected, 're-usable')
+  t.end()
 })
 
 test('expected output coerceIds = false', function (t) {
@@ -55,6 +65,32 @@ test('expected output bounds = false', function (t) {
   ]
   var rs = fs.createReadStream(path.join(__dirname, 'test.osm'))
   rs.pipe(new Osm2Json({types: ['way'], bounds: false})).pipe(concat(function (data) {
+    t.deepEqual(data, expected)
+    t.end()
+  }))
+})
+
+test('diffResult', function (t) {
+  var input = '<diffResult generator="OpenStreetMap Server" version="0.6">' +
+    '<node old_id="1" new_id="2" new_version="2"/>' +
+    '<way old_id="3" new_id="4" new_version="2"/>' +
+    '<relation old_id="5"/>' +
+    '</diffResult>'
+  var expected = [{
+    type: 'node',
+    old_id: 1,
+    new_id: 2,
+    new_version: 2
+  }, {
+    type: 'way',
+    old_id: 3,
+    new_id: 4,
+    new_version: 2
+  }, {
+    type: 'relation',
+    old_id: 5
+  }]
+  fromString(input).pipe(new Osm2Json()).pipe(concat(function (data) {
     t.deepEqual(data, expected)
     t.end()
   }))
